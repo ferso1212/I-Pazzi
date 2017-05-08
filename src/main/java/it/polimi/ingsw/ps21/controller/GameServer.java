d4b6d6e9920351769d83aa38b0934ac166a27259
Daniele <DanielWhite95@users.noreply.github.com>
2017-05-08 18:29:18 +0200

    Created Controller Package

---
+++ src/main/java/it/polimi/ingsw/ps21/controller/GameServer.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.ps21.controller;
+
+import java.util.Queue;
+
+import it.polimi.ingsw.ps21.model.*;
+
+public class GameServer {
+	
+	final private int TCP_PORT=12300;
+	private int timeout; // Timeout in secondi prima di avviare una partita
+	private Queue<Player> pendingPlayer;
+	private Queue<Player> pendingAdvancedPlayer;
+	private Queue<Socket> pendingSocket;
+	
+	public int getPortNumber(){
+		return TCP_PORT;
+		
+	}
+	
+	public void addOnQueue(Player newPlayer, int type){
+		
+	}
+
+}
