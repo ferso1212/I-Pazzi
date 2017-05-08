132053cbd51fb0745bbcdf3dd2c914359270abff
Fabrizio <fabrizio.indirli@mail.polimi.it>
2017-05-08 21:29:26 +0200

    Added MembersColor and CardType enums

---
+++ src/main/java/it/polimi/ingsw/ps21/CardType.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.ps21;
+
+/**Used to enumerate the different types of the cards
+ * The game contains 6 different types of cards:
+ * <li>Territory Cards (Green cards)
+ * <li> Building Cards (Yellow cards)
+ * <li> Character Cards (Blue cards)
+ * <li> Venture Cards (Purple cards)
+ * <li> Excommunication cards
+ * <li> Leader cards (used in Advanced Matches)
+ * @author fabri
+ *
+ */
+public enum CardType {TERRITORY, BUILDING, CHARACTER, VENTURE, EXCOMMUNICATION, LEADER,
+
+}
