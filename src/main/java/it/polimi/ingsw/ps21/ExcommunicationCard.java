347b51452b936e34a2ba886936554d8bc272ef7d
Fabrizio <fabrizio.indirli@mail.polimi.it>
2017-05-08 21:25:37 +0200

    added initial draft of ExcommunicationCard class

---
+++ src/main/java/it/polimi/ingsw/ps21/ExcommunicationCard.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.ps21;
+
+/**Used to model Excommunication Cards.
+ * @author fabri
+ *
+ */
+public class ExcommunicationCard {
+	protected int cardId;
+	protected int cardEra;
+	
+	public int getId()
+	{
+		return this.cardId;
+	}
+	
+	public int getEra()
+	{
+		return this.getEra();
+	}
+}
