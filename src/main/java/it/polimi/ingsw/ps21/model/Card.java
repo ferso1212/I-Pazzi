07ee2d3a0d5eaca0a5c8d38081dab4035f3710a5
Daniele <DanielWhite95@users.noreply.github.com>
2017-05-20 10:52:40 +0200

    Added Subdeck class and fixed some classes

--- src/main/java/it/polimi/ingsw/ps21/model/Card.java
+++ src/main/java/it/polimi/ingsw/ps21/model/Card.java
@@ -5,14 +5,21 @@ import java.util.ArrayList;
 public abstract class Card {
 	protected String name;
 	protected ArrayList<Requirement> requires;
+	protected Requirement chosenReq;	
+	public Card(String name, Requirement req){
+		this.name = name;
+		this.requires = new ArrayList<Requirement>();
+		chosenReq = req;
+		requires.add(req);
+	}
 	
 	public Card(String name, Requirement reqs[]){
 		this.name = name;
 		this.requires = new ArrayList<Requirement>();
-		
+		for (Requirement r: reqs)	{
+			requires.add(r);
+		}
 	}
 	
-	public Requirement getRequirement(){
-		return requires.a
-	}
+	public abstract Requirement getRequirement();
 }
--- src/main/java/it/polimi/ingsw/ps21/model/Deck.java
+++ src/main/java/it/polimi/ingsw/ps21/model/Deck.java
@@ -3,13 +3,20 @@ package it.polimi.ingsw.ps21.model;
 import java.util.ArrayList;
 
 public class Deck {
-	private ArrayList<TerritoryCard> greenCards[];
-	private ArrayList<BuildingCard> yellowCards[];
-	private ArrayList<CharacteerCard> blueCards[];
-	private ArrayList<VentureCard> purpleCards[];
+	private SubDeck<TerritoryCard> greenCards;
+	private SubDeck<BuildingCard> yellowCards;
+	private SubDeck<CharacterCard> blueCards;
+	private SubDeck<VentureCard> purpleCards;
 	
 	public Deck(String fileConfiguration){
-		
+		greenCards = new SubDeck<TerritoryCard>();
+		yellowCards = new SubDeck<BuildingCard>();
+		blueCards = new SubDeck<CharacterCard>();
+		purpleCards = new SubDeck<VentureCard>();
+	}
+	
+	public void shuffle(){
+	
 	}
 	
 }
