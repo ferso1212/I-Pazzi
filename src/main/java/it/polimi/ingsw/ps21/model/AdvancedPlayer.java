127e462ca7934c6fab564d499714c5acab6d487e
Fabrizio <fabrizio.indirli@mail.polimi.it>
2017-05-20 12:30:49 +0200

    Added AdvancedModifier and AdvancedPlayer classes

---
+++ src/main/java/it/polimi/ingsw/ps21/model/AdvancedPlayer.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.ps21.model;
+
+public class AdvancedPlayer extends Player {
+	private ArrayList<LeaderCards> leaderCards;
+	private AdvancedModifier advModifier;
+	
+	public ArrayList<LeaderCards> getActivatedLeaderCards()
+	{
+		
+	}
+	
+	public LeaderCard chooseLeaderCardToCopy()
+	{
+		
+	}
+	
+	public AdvancedModifier getAdvMod()
+	{
+		return this.advModifier;
+	}
+	
+}
