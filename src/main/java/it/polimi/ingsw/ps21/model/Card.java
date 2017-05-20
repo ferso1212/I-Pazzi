26c040741aa6c804161a8957ec4f4469d27ffe37
Fabrizio <fabrizio.indirli@mail.polimi.it>
2017-05-20 12:27:26 +0200

    Fixed references to 'Requirement' instead of 'Requirements' in Card class

--- src/main/java/it/polimi/ingsw/ps21/model/Card.java
+++ src/main/java/it/polimi/ingsw/ps21/model/Card.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 
 public abstract class Card {
 	protected String name;
-<<<<<<< HEAD
-	protected ArrayList<Requirement> requires;
-	protected Requirement chosenReq;	
-	public Card(String name, Requirement req){
+
+	protected ArrayList<Requirements> requires;
+	protected Requirements chosenReq;	
+	public Card(String name, Requirements req){
 		this.name = name;
-		this.requires = new ArrayList<Requirement>();
+		this.requires = new ArrayList<Requirements>();
 		chosenReq = req;
 		requires.add(req);
 		
 	public Card(String name, Requirements reqs[]){
 		this.name = name;
-		this.requires = new ArrayList<Requirement>();
-		for (Requirement r: reqs)	{
+		this.requires = new ArrayList<Requirements>();
+		for (Requirements r: reqs)	{
 			requires.add(r);
 		}
 	}
 	
-	public abstract Requirement getRequirement();
+	public abstract Requirements getRequirement();
 }
