e3f98b7f8b1cef2eae47168360f1ec5f5ad01151
Fersini <dario.fersini@mail.polimi.it>
2017-05-22 00:05:48 +0200

    Update code and UML

--- src/main/java/it/polimi/ingsw/ps21/model/WorkAction.java
+++ src/main/java/it/polimi/ingsw/ps21/model/WorkAction.java
@@ -31,8 +31,8 @@ public class WorkAction extends Action {
 	@Override
 	public boolean execute() {
 		boolean controlOccupation = space.occupy(this.player);
-		boolean controlEffect;
-		ArrayList<DevelopmentCard> cardToActivate;
+		boolean controlEffect = false;
+		ArrayList<DevelopmentCard> cardToActivate = null;
 		switch (space.getType()) {
 		case HARVEST:
 		{
@@ -45,26 +45,47 @@ public class WorkAction extends Action {
 			break;}
 		case PRODUCTION:
 		{
+			if(space instanceof SingleProductionSpace){
+				cardToActivate = player.getWorkCards(famMember.getValue(), WorkType.PRODUCTION);
+			}
+			else if (space instanceof MultipleProductionSpace){
+				cardToActivate = player.getWorkCards(famMember.getValue() - 3, WorkType.PRODUCTION);
+			}
 			break;
 		}
-			
+		case TOWER:
+		{
+			return false;
+		}
+		case COUNCIL:
+		{
+			return false;
+		}
+		default: cardToActivate = null;
 		}
 		int i;
-		for(i=0; i<cardToActivate.size(); i++){
-			Effect[] effetti = cardToActivate.get(i).getEffect();
-			if ((effetti[0]==null) && (effetti[1]==null)){ //caso in cui non ci sono effetti permanenti
-				controlEffect=true;
-			}
-			else if ((effetti[0]==null)){
-				controlEffect=effetti[1].activate(player);
-			}
-			else if ((effetti[1]==null)){
-				controlEffect=effetti[0].activate(player);
-			}
-			else if ((effetti[0]!=null) && (effetti[1]!=null)){
-				//implementazione della scelta dell'effetto
+		if (cardToActivate != null){
+			for(i=0; i<cardToActivate.size(); i++){
+				Effect[] effetti = cardToActivate.get(i).getEffect();
+				if ((effetti[0]==null) && (effetti[1]==null)){ //caso in cui non ci sono effetti permanenti
+					controlEffect=true;
+				}
+				else if ((effetti[0]==null)){
+					controlEffect=effetti[1].activate(player);
+				}
+				else if ((effetti[1]==null)){
+					controlEffect=effetti[0].activate(player);
+				}
+				else if ((effetti[0]!=null) && (effetti[1]!=null)){
+					controlEffect=true;//implementazione della scelta dell'effetto
+				}
 			}
 		}
+		else controlEffect=true;
+		if(controlOccupation && controlEffect){
+			return true;
+		}
+		return false;
 		
 	}
 	
