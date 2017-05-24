23ab5953887bf76eaebe44d48548a210baf38ea7
Fabrizio <fabrizio.indirli@mail.polimi.it>
2017-05-24 15:09:51 +0200

    Added modifier in Property class, updated UML

--- src/main/java/it/polimi/ingsw/ps21/model/Property.java
+++ src/main/java/it/polimi/ingsw/ps21/model/Property.java
@@ -1,14 +1,19 @@
 package it.polimi.ingsw.ps21.model;
 
-/**Used to store datas of resources and points.
+/**Used to store datas of resources and points. 
+ * The following datas are stored for each property: 
+ * <li>the <b>id</b>, which is a string that identifies the property;
+ * <li>the <b>value</b>, which is an integer value that describes the quantity of that property;
+ * <li>the <b>modifier</b>, which is an integer value that is added to the quantity to pay each time the payValue(num) method is called.
  * @author fabri
  *
  */
 public class Property {
 	private String id;
 	private int value;
+	private int modifier;
 	
-	/**Constructs a Property object
+	/**Constructs a Property object with custom id and initial value. The modifier is automatically set to 0.
 	 * 
 	 * @param id the name (a string) of the property
 	 * @param initialValue the initial value of the property
@@ -17,7 +22,7 @@ public class Property {
 	{
 		this.id=new String(id);
 		this.value=initialValue;
-		
+		this.modifier=0;
 	}
 	
 	/**Returns the value of the property.
@@ -58,10 +63,12 @@ public class Property {
 	 * @param num
 	 * @return true if the operation succeeded, false if the 'num' parameter is negative
 	 */
-	public boolean subValue(int num)
+	public boolean payValue(int num)
 	{
 		if(num<0) return false; //this method can't be used to add values
-		this.value-=num;
+		int modifiedNum = num + this.modifier; //adds the modifier value to the value that should be payed
+		if(this.value<modifiedNum) return false; 
+		this.value-=modifiedNum;
 		return true;
 	}
 	
@@ -74,5 +81,20 @@ public class Property {
 	{
 		if(this.value>=num) return true;
 		else return false;
+	}
+
+	/**Sets the modifier
+	 * @param modifier the value of the modifier to set
+	 */
+	public void setModifier(int modifier) {
+		this.modifier = modifier;
+	}
+	
+	/**Used to increase the value of the modifier  by num units.
+	 * If num<0, the value of the modifier is reduced by |num| units.
+	 * @param num the number to add to the modifier value
+	 */
+	public void increaseModifier(int num) {
+		this.modifier += num;
 	}
 }
