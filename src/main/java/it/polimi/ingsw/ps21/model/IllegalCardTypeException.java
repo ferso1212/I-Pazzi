3fd08040f5d21f416c8c783b18c493a1059d936b
Daniele <DanielWhite95@users.noreply.github.com>
2017-05-27 11:55:49 +0200

    Added OrRequirement class

---
+++ src/main/java/it/polimi/ingsw/ps21/model/IllegalCardTypeException.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.ps21.model;
+
+public class IllegalCardTypeException extends Exception {
+
+}
---
+++ src/main/java/it/polimi/ingsw/ps21/model/IllegalChoiceException.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.ps21.model;
+
+public class IllegalChoiceException extends Exception {
+
+}
