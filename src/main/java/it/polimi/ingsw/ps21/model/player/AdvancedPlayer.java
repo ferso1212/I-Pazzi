627db6d27c5f9898fd39c3667b1d4ba4062dab6f
Fabrizio <fabrizio.indirli@mail.polimi.it>
2017-05-28 11:31:57 +0200

    Implemented some AdvancedPlayer methods

--- src/main/java/it/polimi/ingsw/ps21/model/player/AdvancedPlayer.java
+++ src/main/java/it/polimi/ingsw/ps21/model/player/AdvancedPlayer.java
@@ -5,27 +5,38 @@ import it.polimi.ingsw.ps21.model.deck.LeaderCard;
 
 
 public class AdvancedPlayer extends Player {
-	public AdvancedPlayer(String name, PlayerProperties properties, int id) {
-		super(name, properties, id);
-		// TODO Auto-generated constructor stub
-	}
-
+	
 	private ArrayList<LeaderCard> leaderCards;
 	private AdvancedModifier advModifier;
 	
+	public AdvancedPlayer(String name, PlayerProperties properties, int id) {
+		super(name, properties, id);
+	}
+
+	
 	public ArrayList<LeaderCard> getActivatedLeaderCards()
 	{
-		
+		ArrayList<LeaderCard> output= new ArrayList<LeaderCard>();
+		for(LeaderCard card: leaderCards)
+		{
+			if(card.isActivated()) output.add(card);
+		}
+		return output;
 	}
 	
-	public LeaderCard chooseLeaderCardToCopy()
+	//TODO
+	public LeaderCard chooseLeaderCardToCopy(LeaderCard[] activeLeaders)
 	{
-		
+		LeaderCard chosen;
+		chosen=activeLeaders[0]; //stub
+		return chosen;
 	}
 	
 	public AdvancedModifier getAdvMod()
 	{
 		return this.advModifier;
 	}
+	
+	public void 
 	
 }
