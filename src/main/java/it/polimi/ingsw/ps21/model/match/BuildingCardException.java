b04e70f479a30e864c3c760cc8cd9b4be94fbbbc
Daniele <DanielWhite95@users.noreply.github.com>
2017-05-29 17:40:39 +0200

    Created MatchBuilder class

---
+++ src/main/java/it/polimi/ingsw/ps21/model/match/BuildingCardException.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.ps21.model.match;
+
+public class BuildingCardException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -192694648600838016L;
+
+}
