9357156573b41b89a4e4af1e8336ae9d3a4a18fe
DanielWhite95 <daniele.marsella95@gmail.com>
2017-05-30 20:56:46 +0200

    Added States of Match

---
+++ src/main/java/it/polimi/ingsw/ps21/model/match/InitialRoundMatch.java
@@ -0,0 +1,51 @@
+package it.polimi.ingsw.ps21.model.match;
+
+import it.polimi.ingsw.ps21.model.actions.Action;
+import it.polimi.ingsw.ps21.model.actions.NotExecutableException;
+import it.polimi.ingsw.ps21.model.board.NotOccupableException;
+import it.polimi.ingsw.ps21.model.player.RequirementNotMetException;
+
+public class InitialRoundMatch extends Match {
+
+	
+	public InitialRoundMatch(Match previousState) {
+		super();
+		this.observers = previousState.observers;
+		this.players = previousState.players;
+		this.order = previousState.order;
+		throwDices();
+		notifyObservers();
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public Match makeAction(Action nextAction) {
+		// if (nextAction.isLegal())
+		try {
+			nextAction.execute();
+		} catch (NotExecutableException | NotOccupableException | RequirementNotMetException e) {
+			return this;
+		}
+		order.poll();
+		notifyObservers();
+		if (order.isEmpty()) return nextState();
+		else return this;
+	}
+	
+	private Match nextState(){
+		for (int i=0; i<4; i++){
+			for (Player p: board.getCouncilPalace().getOccupants()));
+			// TODO need a method that return playerOrder in councilPalace
+		}
+		return new FinalRoundMatch(this);
+	}
+
+	@Override
+	public Match getCopy() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
