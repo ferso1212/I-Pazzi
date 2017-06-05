f80b3ad163f68377fe1fbe82c1ee10c7b50696dc
Daniele <DanielWhite95@users.noreply.github.com>
2017-06-05 18:30:15 +0200

    Interface RMIConnectionCreator

--- src/main/java/it/polimi/ingsw/ps21/view/RMIConnectionCreator.java
+++ src/main/java/it/polimi/ingsw/ps21/view/RMIConnectionCreator.java
@@ -4,7 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RMIConnectionCreator extends Remote {
-	
+	 
 	public abstract RMIConnection getNewConnection(String connectionName);
 
 }
