01c860eda81a79621948fc55b4a230e32decc1d6
Daniele <DanielWhite95@users.noreply.github.com>
2017-06-05 19:48:22 +0200

    Fixed interface

--- src/main/java/it/polimi/ingsw/ps21/client/CLInterface.java
+++ src/main/java/it/polimi/ingsw/ps21/client/CLInterface.java
@@ -1,23 +1,86 @@
 package it.polimi.ingsw.ps21.client;
 
-import it.polimi.ingsw.ps21.controller.MatchData;
+import java.io.InputStream;
+import java.util.Scanner;
 
-public class CLInterface {
+import it.polimi.ingsw.ps21.controller.AcceptedAction;
+import it.polimi.ingsw.ps21.controller.BoardData;
+import it.polimi.ingsw.ps21.controller.CostChoice;
+import it.polimi.ingsw.ps21.controller.CouncilChoice;
+import it.polimi.ingsw.ps21.controller.EffectChoice;
+import it.polimi.ingsw.ps21.controller.MatchData;
+import it.polimi.ingsw.ps21.controller.RefusedAction;
+import it.polimi.ingsw.ps21.controller.VaticanChoice;
+
+public class CLInterface implements UserInterface {
 	
 	public MatchData match;
+	public Scanner userInput;
 	
 	public CLInterface() {
-		// TODO Auto-generated constructor stub
+		System.out.println("Match started ...");
+		userInput = new Scanner(System.in);
 	}
 	
-	private void show(MatchData match) {
+	@Override
+	public void updateView(MatchData match, BoardData board) {
 		System.out.print("Stato della partita:\n "
-					+ "Era: " + match.getEra() + "\tRound: " + match.getRound()
-					+ "Tabellone: ");
+					+ "Era: " + match.getEra() + "\tRound: " + match.getRound());
+		System.out.println("\nTabellone: ")
+		
 	}
 
 	public boolean isEnded() {
 		return match.getEra() == 3;
+	}
+
+	@Override
+	public void showInfo(String info) {
+		System.out.println(info);
+	}
+
+	@Override
+	public void reqChoice(VaticanChoice choice) {
+		System.out.println("You to take a choice, bastard:\n ");
+		System.out.println("Do you want to receive an excommunication(1) or not(2)? If you reject excommunication you will loose all your faith points");
+		int userChoice = userInput.nextInt();
+		while(userChoice != 1 && userChoice != 2){
+			System.out.println("Invalid choice");
+			userChoice = userInput.nextInt();
+		}
+		if (userChoice == 1) choice.setChosen(false);
+		else choice.setChosen(true);
+		
+	}
+
+	@Override
+	public void reqChoice(CostChoice choice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reqChoice(CouncilChoice choice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reqChoice(EffectChoice choice) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showMessage(AcceptedAction mess) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showMessage(RefusedAction mess) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/main/java/it/polimi/ingsw/ps21/client/UserInterface.java
+++ src/main/java/it/polimi/ingsw/ps21/client/UserInterface.java
@@ -3,18 +3,40 @@ package it.polimi.ingsw.ps21.client;
 import java.util.Observable;
 import java.util.Observer;
 
+import it.polimi.ingsw.ps21.controller.AcceptedAction;
+import it.polimi.ingsw.ps21.controller.BoardData;
+import it.polimi.ingsw.ps21.controller.CostChoice;
+import it.polimi.ingsw.ps21.controller.CouncilChoice;
+import it.polimi.ingsw.ps21.controller.EffectChoice;
 import it.polimi.ingsw.ps21.controller.MatchData;
+import it.polimi.ingsw.ps21.controller.Message;
+import it.polimi.ingsw.ps21.controller.RefusedAction;
+import it.polimi.ingsw.ps21.controller.VaticanChoice;
 
-public abstract class UserInterface implements Observer {
+public abstract interface UserInterface {
 	
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		if (arg1 instanceof MatchData) 
-         show((MatchData) arg1); 
-		
-	}
-
-	public abstract void show(MatchData matchInfo);
+	
+	public abstract void updateView(MatchData match, BoardData board);
+	
+	// public abstract ActionData reqAction();
+	
+	public abstract void showInfo(String name);
+	
+	public abstract void reqChoice(VaticanChoice choice);
+	
+	public abstract void reqChoice(CostChoice choice);
+	
+	public abstract void reqChoice(CouncilChoice choice);
+	
+	public abstract void reqChoice(EffectChoice choice);
+	
+	// public abstract void reqChoice(LeaderChoice choice);
+	
+	// public abstract void reqChoice(WorkChoice choice);
+	
+	public abstract void showMessage(AcceptedAction mess);
+	
+	public abstract void showMessage(RefusedAction mess);
 
 
 
