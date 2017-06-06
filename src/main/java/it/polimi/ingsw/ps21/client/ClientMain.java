8b7184489baf928542e84c6cc35a0a94f96882d4
Fabrizio <fabrizio.indirli@mail.polimi.it>
2017-06-06 11:08:27 +0200

    Updated ClientMain

--- src/main/java/it/polimi/ingsw/ps21/client/ClientMain.java
+++ src/main/java/it/polimi/ingsw/ps21/client/ClientMain.java
@@ -9,6 +9,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
@@ -31,7 +32,11 @@ public class ClientMain {
     	while(chosenConnection!=1 && chosenConnection!=2)
     	{
     	System.out.println("\nChoose the connection method to use: \n1 Socket \n2 RMI");
-    	chosenConnection= in.nextInt();
+    	try {
+			chosenConnection= in.nextInt();
+		} catch (InputMismatchException e) {
+			chosenConnection=0;
+		}
     	in.nextLine();
     	if((chosenConnection!=1 && chosenConnection!=2)) System.out.println("\nInvalid choice.");
     	}
@@ -43,7 +48,11 @@ public class ClientMain {
     	while(chosenRules!=1 && chosenRules!=2)
     	{
     	System.out.println("\nChoose the rules that you want to use in the game: \n1 Standard \n 2 Advanced");
-    	chosenRules= in.nextInt();
+    	try {
+			chosenRules= in.nextInt();
+		} catch (InputMismatchException e) {
+			chosenRules=0;
+		}
     	if(chosenRules!=1 && chosenRules!=2) System.out.println("\nInvalid choice.");
     	}
     	
