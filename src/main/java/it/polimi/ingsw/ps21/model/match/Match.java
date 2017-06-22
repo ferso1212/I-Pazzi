ce3de3d04b92dad5ebb3b44f9e8a36f31b7d1af0
Daniele <DanielWhite95@users.noreply.github.com>
2017-06-22 09:45:40 +0200

    Added advanced match

--- src/main/java/it/polimi/ingsw/ps21/model/match/Match.java
+++ src/main/java/it/polimi/ingsw/ps21/model/match/Match.java
@@ -38,44 +38,18 @@ import it.polimi.ingsw.ps21.model.properties.PropertiesId;
  * @author gullit
  *
  */
-public class Match extends Observable {
+public abstract class Match extends Observable {
 	
-	private EnumMap<PlayerColor, Player> players;
-	private ArrayList<Player> order;
-	protected ArrayList<ExtraAction> extraActions;
 	protected Board board;
 	protected int orangeDice;
 	protected int blackDice;
 	protected int whiteDice;
 	protected int period; 
 	protected RoundType round;
-	private boolean ended = false;
-	private int currentPlayer;
+	protected boolean ended = false;
+	protected int currentPlayer;
 	
-	public Match(PlayerColor...colors)throws InvalidIDException, BuildingDeckException{
-		if (colors.length>4) throw new InvalidIDException();
-		MatchFactory builder = MatchFactory.instance();
-		ImmProperties[] initialProperties = builder.makeInitialProperties();
-		players = new EnumMap<>(PlayerColor.class);
-		order = new ArrayList<>();
-		ArrayList<Player> tempPlayer = new ArrayList<>();
-		board = new Board(colors.length, false);
-		board.getDeck().shuffle();
-		extraActions = new ArrayList<>();
-		for (int i=0; i<colors.length; i++){
-			players.put(colors[i], new Player(colors[i], new PlayerProperties(0)));
-			players.get(colors[i]).getProperties().increaseProperties(initialProperties[i]);
-			tempPlayer.add(players.get(colors[i]));
-		}
-		for (int j=0; j<4; j++)
-			for (int i=0; i<tempPlayer.size(); i++)
-				order.add(tempPlayer.get(i));
-		currentPlayer=0;
-		period = 1;
-		round = RoundType.INITIAL_ROUND;
-		throwDices();
-		board.newSetBoard(period);
-		setChanged();
+	public Match()throws InvalidIDException, BuildingDeckException{
 	}
 	
 	public Match(Match previousMatch){
@@ -83,8 +57,6 @@ public class Match extends Observable {
 		this.blackDice = previousMatch.blackDice;
 		this.orangeDice = previousMatch.orangeDice;
 		this.whiteDice = previousMatch.whiteDice;
-		this.players = previousMatch.players;
-		this.order = previousMatch.order;
 		this.period = previousMatch.period;
 		this.round = previousMatch.round;
 		this.ended = previousMatch.ended;
@@ -97,11 +69,6 @@ public class Match extends Observable {
 		orangeDice = (int) generator.nextInt(5) + 1;
 		blackDice = (int) generator.nextInt(5) + 1;
 		whiteDice = (int) generator.nextInt(5) + 1;
-		for (Player p: players.values()){
-			p.getFamily().getMember(MembersColor.ORANGE).setValue(orangeDice);
-			p.getFamily().getMember(MembersColor.ORANGE).setValue(blackDice);
-			p.getFamily().getMember(MembersColor.ORANGE).setValue(whiteDice);
-		}
 	}
 
 	
@@ -121,45 +88,16 @@ public class Match extends Observable {
 		return round;
 	}
 	
-	public Player getCurrentPlayer(){
-		return order.get(currentPlayer);
-	}
+	public abstract Player getCurrentPlayer();
 	
-	public ExtraAction[] doAction(Action action) throws NotExecutableException, RequirementNotMetException, InsufficientPropsException, VaticanRoundException{
-		if (!(round == RoundType.VATICAN_ROUND)){
-			if ((!(action instanceof VaticanAction))){
-				ExtraAction[] extraActionPool;
-				extraActionPool = action.activate(order.get(currentPlayer),this);
-				setChanged();
-				notifyObservers();
-				return extraActionPool;
-		}
-			else throw new VaticanRoundException();
-		}
-		else
-			if (action instanceof VaticanAction){
-				ExtraAction []extraActionPool;
-				extraActionPool = action.activate(order.get(currentPlayer), this);
-				setChanged();
-				notifyObservers();
-				return extraActionPool;
-			}
-			else throw new VaticanRoundException();
-	}
+	public abstract ExtraAction[] doAction(Action action) throws NotExecutableException, RequirementNotMetException, InsufficientPropsException, VaticanRoundException;
 	
 	/**
 	 * @return TRUE if no actions are left in the queue (all players have performed all their placement action in the round)
 	 */
-	public RoundType setNextPlayer(){
-		currentPlayer++;
-		if (currentPlayer == order.size()) nextRound();
-		return round;
-	}
+	public abstract RoundType setNextPlayer();
 
-	public Match getCopy() throws CloneNotSupportedException{
-		Match copied = new Match(this);
-		return copied;
-	}
+	public abstract Match getCopy() throws CloneNotSupportedException;
 	
 	public int[] getDices() {
 		int returnValues[] = new int[3];
@@ -169,116 +107,18 @@ public class Match extends Observable {
 		return returnValues;
 	 }
 	
-	public int getNumberPlayers(){
-		return this.players.size();
-	}
+	public abstract int getNumberPlayers();
 
 
-	public List<ExtraAction> getExtraActions() {
-		return extraActions;
-	}
-	
-	public void vaticanLoop(){
-		// TODO Check this method
-		Queue<Player> supportedPlayers = new ArrayDeque<>();
-		Map<Player, Boolean> supportChoices = new HashMap<Player, Boolean>();
-		for(Player p: players.values())
-		{	
-		if (board.getExcommunicationRequirement(period) > p.getProperties().getProperty(PropertiesId.FAITHPOINTS).getValue())	
-			{/*setta scomunica*/}
-		else supportedPlayers.add(p);
-		}
-		notifyObservers();
-		// TODO Set nextPlayer
-		supportedPlayers.poll();
-		if (supportedPlayers.isEmpty()){
-		for (Player p: players.values()){
-			if (!(supportChoices.containsKey(p))) return;
-			}
-		nextRound();
-		}
-	}
+	public abstract List<ExtraAction> getExtraActions();
 	
 	public boolean isEnded(){
 		return ended;
 	}
-
-	private void endMatch() {
-		// TODO Check implementation
-		Map<Player, Integer> militaryBonus = calculateMilitaryWinner();
-		Map<Player, Integer> playerPositions = calculateWinner(militaryBonus); 	
-		ended = true;
-		setChanged();
-		notifyObservers();
-	}
-
-	private Map<Player, Integer> calculateMilitaryWinner(){
-		Map<Player, Integer> result = new HashMap<>();
-		//calculate 1 and second value
-		int values[] = new int[players.size()];
-		for (int i=0; i<values.length; i++){
-			values[i]=0;
-		}
-		for (Player p: players.values()){
-			for (int j=0; j<values.length; j++)
-			 if (p.getProperties().getProperty(PropertiesId.MILITARYPOINTS).getValue() >= values[j]) 
-				 {
-				 	values[j] = p.getProperties().getProperty(PropertiesId.MILITARYPOINTS).getValue();
-				  }
-		}
-		int numWinners = 0;
-		for(Player p:players.values()){
-			for (int j=0; j<values.length;j++)
-			if (p.getProperties().getProperty(PropertiesId.MILITARYPOINTS).getValue() == values[j]) 
-			{
-				if(j==0){
-					result.put(p, 1);
-					numWinners ++;
-				}
-				else if ( j== 1 && numWinners<2) result.put(p, 2);
-					else result.put(p, j+1);
-			}
-		}
-		return result;
-	}
 	
-	private Map<Player, Integer> calculateWinner(Map<Player, Integer> militaryBonus) {
-		Map<Player, Integer> result = new HashMap<>();
-		// Calculate value orders
-		int values[] = new int[players.size()];
-		for (int i=0; i<values.length; i++){
-			values[i]=0;
-		}
-		for (Player p: players.values()){
-			for (int j=0; j<values.length; j++)
-			 if (p.getFinalVictoryPoints(board.getTrackBonuses(), board.getCardBonus(), militaryBonus.get(p)) >= values[j]) 
-				 {
-				 	values[j] = p.getFinalVictoryPoints(board.getTrackBonuses(), board.getCardBonus(), militaryBonus.get(p));
-				  }
-		}
-		for(Player p:players.values()){
-			for (int j=0; j<values.length;j++)
-			if (p.getFinalVictoryPoints(board.getTrackBonuses(), board.getCardBonus(), militaryBonus.get(p)) == values[j]) 
-				result.put(p, j+1);
-		}
-		return null;
-	}
+	public abstract PlayerColor[] getOrderQueue();
 	
-	public PlayerColor[] getOrderQueue()
-	{
-		Player[] players= order.toArray(new Player[0]);
-		PlayerColor[] output= new PlayerColor[players.length];
-		for (int i=0; i<players.length; i++)
-		{
-			output[i]= players[i].getId();
-		}
-		return output;
-	}
-	
-	public Collection<Player> getPlayers()
-	{
-		return players.values();
-	}
+	public abstract Collection<Player> getPlayers();
 	
 	public int getOrangeDice() {
 		return orangeDice;
@@ -292,48 +132,7 @@ public class Match extends Observable {
 		return blackDice;
 	}
 	
-	public void nextRound(){
-		if (round == RoundType.INITIAL_ROUND) round = RoundType.FINAL_ROUND;
-		else if (round == RoundType.FINAL_ROUND) round = RoundType.VATICAN_ROUND;
-		else if (round == RoundType.VATICAN_ROUND){ 
-			if (period <3) round = RoundType.INITIAL_ROUND;
-			else {
-				endMatch();
-				return;
-			}
- 		}
-		currentPlayer = 1;
-		Queue<FamilyMember> temp = board.getCouncilPalace().getOccupants();
-		ArrayList<Player> oldOrder = new ArrayList<>();
-		for (int i=0; i< players.values().size(); i++){
-			oldOrder.add(order.get(i));
-		}
-		ArrayList<Player> newOrder = new ArrayList<>();
-		for (FamilyMember f: temp){
-			Player player = players.get(f.getOwnerId());
-			if (order.contains(player));
-			else {	
-				newOrder.add(player);
-				oldOrder.remove(player);
-			}
-		}
-		for (Player p: oldOrder){
-			newOrder.add(p);
-		}
-		order = new ArrayList<>();
-		for ( int j = 0 ; j < newOrder.size(); j++) order.add(newOrder.get(j));
-		if (round != RoundType.VATICAN_ROUND){
-			for (int i=0; i<3; i++)
-				for ( int j =0 ; j< newOrder.size(); j++) order.add(newOrder.get(j));
-			board.newSetBoard(period);
-			for (Player p: players.values()){
-				p.getFamily().roundReset();
-			}
-		}
-		setChanged();
-		notifyObservers();
-		
-	}
+    public abstract void nextRound();
 
 	
 }
--- src/main/java/it/polimi/ingsw/ps21/model/match/SimpleMatch.java
+++ src/main/java/it/polimi/ingsw/ps21/model/match/SimpleMatch.java
@@ -2,7 +2,6 @@ package it.polimi.ingsw.ps21.model.match;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
@@ -12,9 +11,9 @@ import java.util.Queue;
 import it.polimi.ingsw.ps21.model.actions.Action;
 import it.polimi.ingsw.ps21.model.actions.ExtraAction;
 import it.polimi.ingsw.ps21.model.actions.NotExecutableException;
+import it.polimi.ingsw.ps21.model.actions.PlayLeaderCard;
 import it.polimi.ingsw.ps21.model.actions.VaticanAction;
 import it.polimi.ingsw.ps21.model.board.Board;
-import it.polimi.ingsw.ps21.model.player.AdvancedPlayer;
 import it.polimi.ingsw.ps21.model.player.FamilyMember;
 import it.polimi.ingsw.ps21.model.player.InsufficientPropsException;
 import it.polimi.ingsw.ps21.model.player.MembersColor;
@@ -24,28 +23,25 @@ import it.polimi.ingsw.ps21.model.player.PlayerProperties;
 import it.polimi.ingsw.ps21.model.player.RequirementNotMetException;
 import it.polimi.ingsw.ps21.model.properties.ImmProperties;
 
-public class AdvancedMatch extends Match {
-
-	private EnumMap<PlayerColor, AdvancedPlayer> players;
-	private ArrayList<AdvancedPlayer> order;
+public class SimpleMatch extends Match {
+	
+	private EnumMap<PlayerColor, Player> players;
+	private ArrayList<Player> order;
 	private ArrayList<ExtraAction> extraActions;
+
 	
-	public AdvancedMatch(Match previousMatch) {
-		super(previousMatch);
-	}
-	
-	public AdvancedMatch(PlayerColor...colors) throws InvalidIDException, BuildingDeckException{
+	public SimpleMatch(PlayerColor...colors) throws InvalidIDException, BuildingDeckException {
 		if (colors.length>4) throw new InvalidIDException();
 		MatchFactory builder = MatchFactory.instance();
 		ImmProperties[] initialProperties = builder.makeInitialProperties();
 		players = new EnumMap<>(PlayerColor.class);
 		order = new ArrayList<>();
-		ArrayList<AdvancedPlayer> tempPlayer = new ArrayList<>();
+		ArrayList<Player> tempPlayer = new ArrayList<>();
 		board = new Board(colors.length, false);
 		board.getDeck().shuffle();
 		extraActions = new ArrayList<>();
 		for (int i=0; i<colors.length; i++){
-			players.put(colors[i], new AdvancedPlayer(colors[i], new PlayerProperties(0)));
+			players.put(colors[i], new Player(colors[i], new PlayerProperties(0)));
 			players.get(colors[i]).getProperties().increaseProperties(initialProperties[i]);
 			tempPlayer.add(players.get(colors[i]));
 		}
@@ -57,32 +53,84 @@ public class AdvancedMatch extends Match {
 		round = RoundType.INITIAL_ROUND;
 		throwDices();
 		board.newSetBoard(period);
-		setChanged();
+		setChanged();		// TODO Auto-generated constructor stub
 	}
 	
-	@Override
+	
+	public SimpleMatch(SimpleMatch simpleMatch){
+		this.blackDice = simpleMatch.blackDice;
+		this.orangeDice = simpleMatch.orangeDice;
+		this.whiteDice = simpleMatch.whiteDice;
+		this.period = simpleMatch.period;
+		this.round = simpleMatch.round;
+		this.board = simpleMatch.board;
+		this.currentPlayer = simpleMatch.currentPlayer;
+		this.ended = simpleMatch.ended;
+		this.extraActions = simpleMatch.extraActions;
+		this.players = simpleMatch.players;
+		this.order = simpleMatch.order;
+			
+	}
+
+
 	public Player getCurrentPlayer(){
 		return order.get(currentPlayer);
 	}
 	
 	@Override
-	public Collection<Player> getPlayers()
-	{
-		ArrayList<Player> result = new ArrayList<>();
-		for (AdvancedPlayer p: players.values()){
-			result.add(p);
-		}
-		return result;
-	}
-	
-	@Override
 	public void throwDices(){
 		super.throwDices();
-		for (AdvancedPlayer p: players.values()){
+		for (Player p: players.values()){
 			p.getFamily().getMember(MembersColor.ORANGE).setValue(orangeDice);
 			p.getFamily().getMember(MembersColor.ORANGE).setValue(blackDice);
 			p.getFamily().getMember(MembersColor.ORANGE).setValue(whiteDice);
 		}
+	}
+	
+	
+	
+	
+	public void nextRound(){
+		if (round == RoundType.INITIAL_ROUND) round = RoundType.FINAL_ROUND;
+		else if (round == RoundType.FINAL_ROUND) round = RoundType.VATICAN_ROUND;
+		else if (round == RoundType.VATICAN_ROUND){ 
+			if (period <3) round = RoundType.INITIAL_ROUND;
+			else {
+				endMatch();
+				return;
+			}
+ 		}
+		currentPlayer = 1;
+		Queue<FamilyMember> temp = board.getCouncilPalace().getOccupants();
+		ArrayList<Player> oldOrder = new ArrayList<>();
+		for (int i=0; i< players.values().size(); i++){
+			oldOrder.add(order.get(i));
+		}
+		ArrayList<Player> newOrder = new ArrayList<>();
+		for (FamilyMember f: temp){
+			Player player = players.get(f.getOwnerId());
+			if (order.contains(player));
+			else {	
+				newOrder.add(player);
+				oldOrder.remove(player);
+			}
+		}
+		for (Player p: oldOrder){
+			newOrder.add(p);
+		}
+		order = new ArrayList<>();
+		for ( int j = 0 ; j < newOrder.size(); j++) order.add(newOrder.get(j));
+		if (round != RoundType.VATICAN_ROUND){
+			for (int i=0; i<3; i++)
+				for ( int j =0 ; j< newOrder.size(); j++) order.add(newOrder.get(j));
+			board.newSetBoard(period);
+			for (Player p: players.values()){
+				p.getFamily().roundReset();
+			}
+		}
+		setChanged();
+		notifyObservers();
+		
 	}
 
 	@Override
@@ -97,48 +145,8 @@ public class AdvancedMatch extends Match {
 	}
 
 	@Override
-	public void nextRound() {
-	if (round == RoundType.INITIAL_ROUND) round = RoundType.FINAL_ROUND;
-	else if (round == RoundType.FINAL_ROUND) round = RoundType.VATICAN_ROUND;
-	else if (round == RoundType.VATICAN_ROUND){ 
-		if (period <3) round = RoundType.INITIAL_ROUND;
-		else {
-			endMatch();
-			return;
-		}
-		}
-	currentPlayer = 1;
-	Queue<FamilyMember> temp = board.getCouncilPalace().getOccupants();
-	ArrayList<AdvancedPlayer> oldOrder = new ArrayList<>();
-	for (int i=0; i< players.values().size(); i++){
-		oldOrder.add(order.get(i));
-	}
-	ArrayList<AdvancedPlayer> newOrder = new ArrayList<>();
-	for (FamilyMember f: temp){
-		AdvancedPlayer player = players.get(f.getOwnerId());
-		if (order.contains(player));
-		else {	
-			newOrder.add(player);
-			oldOrder.remove(player);
-		}
-	}
-	for (AdvancedPlayer p: oldOrder){
-		newOrder.add(p);
-	}
-	order = new ArrayList<>();
-	for ( int j = 0 ; j < newOrder.size(); j++) order.add(newOrder.get(j));
-	if (round != RoundType.VATICAN_ROUND){
-		for (int i=0; i<3; i++)
-			for ( int j =0 ; j< newOrder.size(); j++) order.add(newOrder.get(j));
-		board.newSetBoard(period);
-		for (AdvancedPlayer p: players.values()){
-			p.getFamily().roundReset();
-		}
-	}
-	setChanged();
-	notifyObservers();
-	
-
+	public Collection<Player> getPlayers() {
+		return players.values();
 	}
 	
 	private Map<Player, Integer> calculateWinner(Map<Player, Integer> militaryBonus) {
@@ -177,32 +185,38 @@ public class AdvancedMatch extends Match {
 		return null;
 	}
 
+
 	@Override
 	public List<ExtraAction> getExtraActions() {
 			return extraActions;
 	}
 
-	@Override
-	public int getNumberPlayers() {
-			return this.players.size();
-	}
 
 	@Override
-	public AdvancedMatch getCopy() throws CloneNotSupportedException {
-		AdvancedMatch copied = new AdvancedMatch(this);
-		return copied;
+	public int getNumberPlayers(){
+		return this.players.size();
 	}
+
 
 	@Override
 	public RoundType setNextPlayer() {
-			currentPlayer++;
-			if (currentPlayer == order.size()) nextRound();
-			return round;
+		currentPlayer++;
+		if (currentPlayer == order.size()) nextRound();
+		return round;
 	}
+
+
+	@Override
+	public SimpleMatch getCopy() throws CloneNotSupportedException {
+		SimpleMatch copied = new SimpleMatch(this);
+		return copied;
+	}
+
 
 	@Override
 	public ExtraAction[] doAction(Action action) throws NotExecutableException, RequirementNotMetException,
 			InsufficientPropsException, VaticanRoundException {
+		if (action instanceof PlayLeaderCard) throw new NotExecutableException();
 		if (!(round == RoundType.VATICAN_ROUND)){
 			if ((!(action instanceof VaticanAction))){
 				ExtraAction[] extraActionPool;
@@ -223,5 +237,5 @@ public class AdvancedMatch extends Match {
 			}
 			else throw new VaticanRoundException();
 	}
-	
 }
+
