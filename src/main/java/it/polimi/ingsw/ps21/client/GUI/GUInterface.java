97a7fe9dd079f3bd11e95a06bb256e801519ae29
Fabrizio <fabrizio.indirli@mail.polimi.it>
2017-07-06 14:21:06 +0200

    Removed some vulnerabilities

--- src/main/java/it/polimi/ingsw/ps21/client/GUI/GUInterface.java
+++ src/main/java/it/polimi/ingsw/ps21/client/GUI/GUInterface.java
@@ -554,6 +554,7 @@ public class GUInterface implements UserInterface {
 			} else
 				return new ActionData(ActionType.NULL, MembersColor.NEUTRAL, 0, null, 0, id);
 		} catch (InterruptedException e) {
+			
 			return new ActionData(ActionType.NULL, MembersColor.NEUTRAL, 0, null, 0, id);
 		}
 	}
